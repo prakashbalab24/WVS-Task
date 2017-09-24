eb7253587105b1f810375d4516b73e5593fa8790
prakashbalab24 <prakashbala.b24@gmail.com>
2017-09-24 15:56:26 +0530

    sync logic fixed

--- app/src/main/java/singledevapps/wvstask/sync/NewsJob.java
+++ app/src/main/java/singledevapps/wvstask/sync/NewsJob.java
@@ -8,19 +8,26 @@ import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.NotificationCompat;
+import android.util.Log;
 
 import com.evernote.android.job.Job;
 import com.evernote.android.job.JobRequest;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import singledevapps.wvstask.MainActivity;
 import singledevapps.wvstask.R;
+import singledevapps.wvstask.activities.SplashScreen;
 import singledevapps.wvstask.helper.Apis;
+import singledevapps.wvstask.model.News;
+import singledevapps.wvstask.model.data.local.DatabaseHandler;
 import singledevapps.wvstask.model.data.remote.NetworkCalls;
 import singledevapps.wvstask.model.data.remote.NewsSouce;
 import singledevapps.wvstask.helper.SharedPrefHelper;
@@ -30,6 +37,7 @@ public class NewsJob extends Job {
 
     public static final String TAG = "newsJob";
     private Context context;
+    private List<News> newsList;
 
     public NewsJob(Context context){
         this.context = context;
@@ -53,13 +61,45 @@ public class NewsJob extends Job {
 
         NotificationManagerCompat.from(getContext())
                 .notify(new Random().nextInt(), notification);
+//        for (String aNewsSourceList : NewsSouce.newsSourceList) {
+//            NetworkCalls nc = new NetworkCalls();
+//            String jsonStr = nc.getServerCall(Apis.getHitUrl(aNewsSourceList));
+//            try {
+//                JSONObject jsonObject = new JSONObject(jsonStr);
+//                if (jsonObject.getString("status").equalsIgnoreCase("ok")) {
+//                    SharedPrefHelper.saveJsonOffline(context.getApplicationContext(), jsonStr, aNewsSourceList);
+//                }
+//            } catch (JSONException e) {
+//                e.printStackTrace();
+//
+//            }
+//        }
         for (String aNewsSourceList : NewsSouce.newsSourceList) {
             NetworkCalls nc = new NetworkCalls();
             String jsonStr = nc.getServerCall(Apis.getHitUrl(aNewsSourceList));
             try {
                 JSONObject jsonObject = new JSONObject(jsonStr);
                 if (jsonObject.getString("status").equalsIgnoreCase("ok")) {
-                    SharedPrefHelper.saveJsonOffline(context.getApplicationContext(), jsonStr, aNewsSourceList);
+                    JSONArray jsonArray;
+                    jsonObject = new JSONObject(jsonStr);
+                    jsonArray = jsonObject.getJSONArray("articles");
+                    Log.i("response", "jsonArray:" + jsonArray.toString());
+                    newsList = new ArrayList<>();
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject jsonFromSource = jsonArray.getJSONObject(i);
+                        String title = jsonFromSource.getString("title");
+                        String urlToImage = jsonFromSource.getString("urlToImage");
+                        String description = jsonFromSource.getString("description");
+                        String sourceUrl = jsonFromSource.getString("url");
+                        News model = new News(aNewsSourceList, title, urlToImage, description, sourceUrl);
+                        newsList.add(model);
+                    }
+                    if (newsList.size() > 0) {
+                        //new DatabaseHandler(context).deleteAll();
+                        new DatabaseHandler(context).clearOldNews(aNewsSourceList);
+                    }
+                    new DatabaseHandler(context).addNewsintoTable(newsList);
+
                 }
             } catch (JSONException e) {
                 e.printStackTrace();
@@ -71,7 +111,7 @@ public class NewsJob extends Job {
 
     public static void schedulePeriodic() {
         new JobRequest.Builder(NewsJob.TAG)
-                .setPeriodic(TimeUnit.HOURS.toMillis(8), TimeUnit.MINUTES.toMillis(20))
+                .setPeriodic(TimeUnit.HOURS.toMillis(1), TimeUnit.MINUTES.toMillis(10))
                 .setUpdateCurrent(true)
                 .setPersisted(true)
                 .setRequiredNetworkType(JobRequest.NetworkType.CONNECTED)
