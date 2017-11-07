45e1f5f952d2a9f7b2138e6d4bfc7cf9b03465d1
prakashbalab24 <prakashbala.b24@gmail.com>
2017-11-07 18:02:17 +0530

    changed Splash screen

--- app/src/main/java/singledevapps/wvstask/activities/SplashScreen.java
+++ app/src/main/java/singledevapps/wvstask/activities/SplashScreen.java
@@ -6,6 +6,8 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 import org.json.JSONArray;
@@ -33,6 +35,8 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
         handler = new Handler();
         handler2 = new Handler();
